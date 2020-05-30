8176527069c0ada367a81cc284fb4abcdb6d015c
Shin Maung Maung <ethereal97@gmail.com>
2020-05-31 05:39:08 +0630

    Create main.js

---
+++ docs/assets/js/main.js
@@ -0,0 +1,15 @@
+let uri = new URL(location);
+
+window.addEventListener('load', function() {
+  document.appendChild(createScriptElement('module', '/assets/js/app.js'));
+});
+
+function createScriptElement(type, src) {
+  var el = document.createElement('script');
+  var sl = src.slice(0, 2)
+  el.type = type;
+  el.src = (sl !== '//' || sl !== 'ht') 
+    ? uri.origin + src 
+    : src;
+  return el;
+}
