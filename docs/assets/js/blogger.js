2aed88d0c46d42b40f23c9b0f55c8721fae162f9
Shin Maung Maung <ethereal97@gmail.com>
2020-05-31 06:05:34 +0630

    Create blogger.js

---
+++ docs/assets/js/blogger.js
@@ -0,0 +1,32 @@
+class Blog {
+  constructor(host) {
+    this.url = new URL(host)
+    this.posts = []
+    
+    bindPaths(this)
+  }
+  
+  fetchXML(callback) {
+    console.error('xml fetching method will comming soon.')
+    return
+  }
+  
+  fetchJSON(callback) {
+    fetch(this.feeds.json)
+      .then(response => response.json())
+      .then(data => callback.call(data))
+  }
+}
+
+function bindPaths(blog) {
+  blog.url.path = '/feeds/default'
+  blog.url.search = new URLSearchParams
+  
+  blog.url.search.set('rel', 'rss')
+  blog.feeds.xml = blog.url.toString()
+  
+  blog.url.search.set('rel', 'json')
+  blog.feeds.json = blog.url.toString()
+}
+
+export default class Blog
