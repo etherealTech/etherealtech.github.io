1c9eec4822abe869a93ba6d0ced80893fab35bc8
Shin Maung Maung <ethereal97@gmail.com>
2020-05-31 06:18:27 +0630

    Update blogger.js

--- docs/assets/js/blogger.js
+++ docs/assets/js/blogger.js
@@ -22,7 +22,7 @@ export default class Blog {
 function bindPaths(blog) {
   let u = new URL(blog.url)
   let q = new URLSearchParams
-  u.pathname = '/feeds/default'
+  u.pathname = '/feeds/posts/default'
   
   q.set('rel', 'rss')
   u.search = q
