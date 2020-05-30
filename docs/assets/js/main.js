d826c0a236590863c8ee440cc4ca2d3474e13b91
Shin Maung Maung <ethereal97@gmail.com>
2020-05-31 05:51:45 +0630

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,15 +1,15 @@
-let uri = new URL(location);
+let uri = new URL(location)
 
 window.addEventListener('load', function() {
-  document.appendChild(createScriptElement('module', '/'+ GITHUB_REPO_NAME + '/assets/js/app.js'));
-});
+  document.body.append(
+    createScriptElement('module', '/'+ GITHUB_REPO_NAME + '/assets/js/app.js')
+  )
+})
 
 function createScriptElement(type, src) {
-  var el = document.createElement('script');
+  var el = document.createElement('script')
   var sl = src.slice(0, 2)
-  el.type = type;
-  el.src = (sl !== '//' || sl !== 'ht') 
-    ? uri.origin + src 
-    : src;
-  return el;
+  el.type = type
+  el.src = (sl !== '//' || sl !== 'ht') ? uri.origin + src : src
+  return el
 }
